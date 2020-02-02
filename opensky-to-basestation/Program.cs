438206a3114506c2444a6e36af9a0d2ff6302be6
Andrew Whewell <andrew@whewell.co.uk>
2020-02-02 21:39:53 +0000

    Copy in code from the defunct OpenSky custom VRS plugin

---
+++ opensky-to-basestation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenSkyToBaseStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
