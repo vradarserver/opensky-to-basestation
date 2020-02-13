6f2687a519c6ea43f4dd77ddd85402b5686e4bd5
Andrew Whewell <andrew@whewell.co.uk>
2020-02-13 22:50:51 +0000

    Switched build over to dotnet publish

--- Build.bat
+++ Build.bat
@@ -1,12 +1,5 @@
 @echo off
 
-set                        MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
-if not exist %MSBUILD% set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe"
-if exist %MSBUILD% goto :BUILD
-
-echo Cannot find msbuild.exe
-exit /b 1
-
 :BUILD
 set SOLUTION="%~dp0opensky-to-basestation.sln"
 
@@ -14,18 +7,13 @@ echo Cleaning binaries
 call "%~dp0Clean.bat"
 echo.
 
-echo Restoring NuGet packages
-%MSBUILD% -t:restore %SOLUTION%
-IF ERRORLEVEL 1 GOTO :ELERROR
-echo.
-
 echo Building debug configuration
-%MSBUILD% -t:build -property:Configuration=Debug %SOLUTION%
+dotnet publish -c Debug
 IF ERRORLEVEL 1 GOTO :ELERROR
 echo.
 
 echo Building release configuration
-%MSBUILD% -t:build -property:Configuration=Release %SOLUTION%
+dotnet publish -c Release
 IF ERRORLEVEL 1 GOTO :ELERROR
 echo.
 
